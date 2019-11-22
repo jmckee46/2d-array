ce3258a900629463f7c8f27f0f37243b3b86180e
John McKee <jmckee3@mac.com>
2019-11-22 15:31:09 -0800

    initial commit

---
+++ 2d-array.go
@@ -0,0 +1,10 @@
+package main
+
+func hourglassSum(arr [][]int32) int32 {
+
+	return 0
+}
+
+func main() {
+
+}
---
+++ spec-2d-array_test.go
@@ -0,0 +1,7 @@
+package main
+
+import "testing"
+
+func TestHourglassSum(t *testing.T) {
+
+}
